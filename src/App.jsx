d47c701f803a0de40cf7688a2a2fee2419fce621
Sanjit Majumdar <mazumdarmadhur@gmail.com>
2022-02-28 00:52:46 +0530

    Added components to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Services from './components/services/Services'
+import Skills from './components/skills/Skills'
+import Testimonials from './components/testimonials/Testimonials'
+import Footer from './components/footer/Footer'
+import Contact from './components/contact/Contact'
 
 const App = () => {
   return (
-    <div>Hello</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Services />
+      <Skills />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
