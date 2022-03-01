5d5885f2737c084daffc45bcdf1ae33d435bb5d1
Asus_Sanjit <mazumdarmadhur@gmail.com>
2022-03-01 20:19:59 +0530

    Added Typerwriter package

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import './header.scss';
 import ME from '../../assets/ME.png';
 import HeaderSocials from './HeaderSocials';
+import Typewriter1 from './Typewriter';
+
 
 function Header() {
   return (
     <header>
       <div className='container header__container'>
         <p>Hello</p>
-        <h3>I'm Sanjit Majumdar</h3>
+        <h3><Typewriter1 /></h3>
         <p>I'm a Front-End Developer, UI/UX Designer and Graphic Artist from Nigeria who is passionate about making sure that systems, interfaces, languages and graphics are human-friendly, emotive and clear. </p>
-        <HeaderSocials/>
+        <HeaderSocials />
+        <a href="#" className='scroll__down'>Scroll Down</a>
 
         <div className="me">
-            <img src={ME} alt="me"  />
+          <img src={ME} alt="me" />
         </div>
+
       </div>
     </header>
   )
---
+++ src/components/header/Typewriter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Typewriter} from 'react-simple-typewriter';
+
+function Typewriter1() {
+    return (
+        <div><p>I'm <span style={{ color: '#fcaf16', fontWeight: 'bold' }}>
+            <Typewriter
+                words={['Sanjit Majumdar', 'a Front-End Dev', 'a Freelancer']}
+                loop={Infinity}
+                cursor
+                cursorStyle='|'
+                typeSpeed={150}
+                deleteSpeed={50}
+                delaySpeed={1000}
+            />
+        </span></p>
+        </div>
+    )
+}
+
+export default Typewriter1;
