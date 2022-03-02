78c1e79e03bbf25ac06e2216acc858f77ef9c06c
Asus_Sanjit <mazumdarmadhur@gmail.com>
2022-03-03 02:32:18 +0530

    Added Map Method for Portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,6 +2,38 @@ import React from 'react';
 import './portfolio.scss';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import Portfolio1 from '../../assets/portfolio1.png';
+import Portfolio2 from '../../assets/portfolio2.png';
+import Portfolio3 from '../../assets/portfolio3.png';
+
+const data = [
+    {
+        id: 1,
+        image: Portfolio1,
+        live: 'https://corpodial-infosolutions.netlify.app/',
+        github: 'https://github.com/mazumdarmadhur/Corpodials-Infosolution',
+        title: 'Corpodials Infosolutions',
+        para: 'Website for the client to enhance their online presence.',
+        display: ''
+    },
+    {
+        id: 2,
+        image: Portfolio2,
+        live: 'https://techniche.in/',
+        github: '',
+        title: 'Techniche',
+        para: ' A tech-blog website for a client which provides technology news.',
+        display: 'none'
+    },
+    {
+        id: 3,
+        image: Portfolio3,
+        live: 'https://mazumdarmadhur.github.io/note-app/',
+        github: 'https://github.com/mazumdarmadhur/note-app',
+        title: 'Note/Todo App',
+        para: 'Note App/Todo app build in javascript',
+        display: ''
+    }
+]
 
 function Portfolio() {
     return (
@@ -10,28 +42,35 @@ function Portfolio() {
                 <h2 className="head-text">My Creative <span>Portfolio</span></h2>
 
                 <div className="container  portfolio__container">
-                    <article className='portfolio__item'>
-                        <div className="portfolio__item-image ">
-                            <img src={Portfolio1} alt="" />
+                    {
+                        data.map(({ id, image, live, github, title, para, display }) => {
+                            return (
+                                <article key={id} className='portfolio__item'>
+                                    <div className="portfolio__item-image ">
+                                        <img src={image} alt="" />
 
-                            <div className="app__work-hover app__flex">
-                                <div className=' app__flex'>
-                                    <a href="google.com">
-                                        <AiFillEye />
-                                    </a>
-                                </div>
-                                <div className=' app__flex'>
-                                    <a href="google.com">
-                                        <AiFillGithub />
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="app__work-content app__flex">
-                            <h4 className="bold-text">Corpodails</h4>
-                            <p className="p-text">This is test.</p>
-                        </div>
-                    </article>
+                                        <div className="app__work-hover app__flex">
+                                            <div className=' app__flex'>
+                                                <a href={live} target='_blank' rel="noopener noreferrer">
+                                                    <AiFillEye />
+                                                </a>
+                                            </div>
+                                            <div className=' app__flex' style={{display: display}}>
+                                                <a href={github} target='_blank' rel="noopener noreferrer">
+                                                    <AiFillGithub />
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="app__work-content app__flex">
+                                        <h4 className="bold-text">{title}</h4>
+                                        <p className="p-text">{para}</p>
+                                    </div>
+                                </article>
+                            )
+                        })
+                    }
+
                 </div>
             </div>
         </section>
