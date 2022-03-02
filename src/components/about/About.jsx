adbd45a1a3666a482de063db884dfd40d32d5632
Asus_Sanjit <mazumdarmadhur@gmail.com>
2022-03-02 19:38:35 +0530

    Skill Section Completed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,20 +4,21 @@ import './about.scss'
 const About = () => {
   return (
     <section id='about'>
-      <div className='head__title'>
-        <h2 className="head-text">About <span>ME</span></h2>
-      </div>
+      <div className='about__section'>
+        <div className='head__title'>
+          <h2 className="head-text">About <span>ME</span></h2>
+        </div>
 
-      <div className='about__text'>
-        <p>
-          Hey! I am <span> Sanjit Majumdar</span>. A 25 year old Web Developer, Freelancer & a Marketing Automation Specialist based in Kanpur, India🌍. <br />
-          I am an Endless seeker of Knowledge🧠. I speak, read and write in Hindi & English. I am constantly learning Web technologies and other design related topics, currently playing around with React👩🏾‍💻. <br />
-          When I am not tapping the keys on the keyboard, you'll find me watching YouTube videos💻 or listening to music🎧. <br />
-          <br />
-          I am fully committed to <span>creating</span> aesthetically <span>beautiful</span>, <span>responsive</span> and <span>usable products</span> 💻. I am skilled in many areas of design and development😎 and I am always excited to do business with like minded people, lets discuss over coffee.
-        </p>
+        <div className='about__text'>
+          <p>
+            Hey! I am <span> Sanjit Majumdar</span>. A 25 year old Web Developer, Freelancer & a Marketing Automation Specialist based in Kanpur, India🌍. <br />
+            I am an Endless seeker of Knowledge🧠. I speak, read and write in Hindi & English. I am constantly learning Web technologies and other design related topics, currently playing around with React👩🏾‍💻. <br />
+            When I am not tapping the keys on the keyboard, you'll find me watching YouTube videos💻 or listening to music🎧. <br />
+            <br />
+            I am fully committed to <span>creating</span> aesthetically <span>beautiful</span>, <span>responsive</span> and <span>usable products</span> 💻. I am skilled in many areas of design and development😎 and I am always excited to do business with like minded people, lets discuss over coffee.
+          </p>
+        </div>
       </div>
-
     </section>
   )
 }
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,9 +1,152 @@
 import React from 'react'
 import './skills.scss'
+import Flutter from '../../assets/flutter.png'
+import ReactPNG from '../../assets/react.png'
+import JS from '../../assets/javascript.png'
+import HTML from '../../assets/html.png'
+import Git from '../../assets/git.png'
+import CSS from '../../assets/css.png'
+import Bootstrap from '../../assets/bootstrap.png'
+import Java from '../../assets/java.png'
+import Marketo from '../../assets/marketo.png'
+import Salesforce from '../../assets/salesforce.png'
+import Figma from '../../assets/figma.png'
 
 function Skills() {
   return (
-    <section id='skills'>Skills</section>
+    <section id='skills' className='app__skills'>
+      <div className='skills__section'>
+        <div className='head__title'>
+          <h2 className="head-text">Skills & Experiences</h2>
+        </div>
+
+        <div className="app_skills_container">
+          <div className="test">
+            
+            {/* React  */}
+            <div className='app__skills-item app__flex'>
+              <div
+                className="app__flex"
+                style={{ backgroundColor: '#fff'}}
+              >
+                <img src={ReactPNG} alt='React' />
+              </div>
+              <p className="p-text">React</p>
+            </div>
+
+            {/* Javascript */}
+            <div className='app__skills-item app__flex'>
+              <div
+                className="app__flex"
+                style={{ backgroundColor: '#fff' }}
+              >
+                <img src={JS} alt='Javascript' />
+              </div>
+              <p className="p-text">Javascript</p>
+            </div>
+
+            {/* Flutter  */}
+            <div className='app__skills-item app__flex'>
+              <div
+                className="app__flex"
+                style={{ backgroundColor: '#fff' }}
+              >
+                <img src={Flutter} alt='Flutter' />
+              </div>
+              <p className="p-text">Flutter</p>
+            </div>
+
+            {/* HTML */}
+            <div className='app__skills-item app__flex'>
+              <div
+                className="app__flex"
+                style={{ backgroundColor: '#fff' }}
+              >
+                <img src={HTML} alt='HTML5' />
+              </div>
+              <p className="p-text">HTML5</p>
+            </div>
+
+            {/* CSS */}
+            <div className='app__skills-item app__flex'>
+              <div
+                className="app__flex"
+                style={{ backgroundColor: '#fff' }}
+              >
+                <img src={CSS} alt='CSS3' />
+              </div>
+              <p className="p-text">CSS3</p>
+            </div>
+
+            {/* Bootstrap  */}
+            <div className='app__skills-item app__flex'>
+              <div
+                className="app__flex"
+                style={{ backgroundColor: '#fff' }}
+              >
+                <img src={Bootstrap} alt='Bootstrap' />
+              </div>
+              <p className="p-text">Bootstrap</p>
+            </div>
+
+            {/* Git */}
+            <div className='app__skills-item app__flex'>
+              <div
+                className="app__flex"
+                style={{ backgroundColor: '#fff' }}
+              >
+                <img src={Git} alt='Git' />
+              </div>
+              <p className="p-text">Git</p>
+            </div>
+
+            {/* JAVA  */}
+            <div className='app__skills-item app__flex'>
+              <div
+                className="app__flex"
+                style={{ backgroundColor: '#fff' }}
+              >
+                <img src={Java} alt='Java' />
+              </div>
+              <p className="p-text">Java</p>
+            </div>
+
+            {/* Marketo  */}
+            <div className='app__skills-item app__flex'>
+              <div
+                className="app__flex"
+                style={{ backgroundColor: '#fff' }}
+              >
+                <img src={Marketo} alt='Marketo' />
+              </div>
+              <p className="p-text">Marketo</p>
+            </div>
+
+            {/* Salesforce  */}
+            <div className='app__skills-item app__flex'>
+              <div
+                className="app__flex"
+                style={{ backgroundColor: '#fff' }}
+              >
+                <img src={Salesforce} alt='Salesforce' />
+              </div>
+              <p className="p-text">Salesforce</p>
+            </div>
+
+            {/* Figma  */}
+            <div className='app__skills-item app__flex'>
+              <div
+                className="app__flex"
+                style={{ backgroundColor: '#fff' }}
+              >
+                <img src={Figma} alt='Figma' />
+              </div>
+              <p className="p-text">Figma</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
