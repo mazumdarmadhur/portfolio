c0ebb5d9e147f3b9c074f46cb627d063b5854a70
Asus_Sanjit <mazumdarmadhur@gmail.com>
2022-03-03 13:31:31 +0530

    Created the testimonial section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import './testimonials.scss'
+import HTML from '../../assets/html.png';
 
 function Testimonials() {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <div className="portfolio__section">
+        <h2 className="head-text">Testimonials</h2>
+
+        <div className='app__testimonial app_flex'>
+          <div className="app__testimonial-item app__flex">
+            <img src={HTML} alt='' />
+            <div className="app__testimonial-content">
+              <p className="p-text">Micael is an awesome developer! I am a backend developer with a passion for building beautiful and functional web applications.</p>
+              <div>
+                <h4 className="bold-text">Rahul</h4>
+                <h5 className="p-text">Corpodials</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="app__testimonial-item app__flex">
+          <img src={HTML} alt='' />
+          <div className="app__testimonial-content">
+            <p className="p-text">Micael is an awesome developer! I am a backend developer with a passion for building beautiful and functional web applications.</p>
+            <div>
+              <h4 className="bold-text">Rahul</h4>
+              <h5 className="p-text">Corpodials</h5>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
