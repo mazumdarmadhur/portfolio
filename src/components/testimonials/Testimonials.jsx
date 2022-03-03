4d676c88fd9b5d6e6f4f79df21927a5f855e90ff
Asus_Sanjit <mazumdarmadhur@gmail.com>
2022-03-03 14:06:28 +0530

    Fixed the Style of the testimonial

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,13 +2,27 @@ import React from 'react'
 import './testimonials.scss'
 import HTML from '../../assets/html.png';
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [{
+  
+}]
+
 function Testimonials() {
   return (
     <section id='testimonials'>
       <div className="portfolio__section">
         <h2 className="head-text">Testimonials</h2>
 
-        <div className='app__testimonial app_flex'>
+        <Swiper className='container testimonials__container'>
+          <SwiperSlide>
           <div className="app__testimonial-item app__flex">
             <img src={HTML} alt='' />
             <div className="app__testimonial-content">
@@ -19,20 +33,20 @@ function Testimonials() {
               </div>
             </div>
           </div>
-        </div>
-
-
-        <div className="app__testimonial-item app__flex">
-          <img src={HTML} alt='' />
-          <div className="app__testimonial-content">
-            <p className="p-text">Micael is an awesome developer! I am a backend developer with a passion for building beautiful and functional web applications.</p>
-            <div>
-              <h4 className="bold-text">Rahul</h4>
-              <h5 className="p-text">Corpodials</h5>
+          </SwiperSlide>
+          <SwiperSlide>
+          <div className="app__testimonial-item app__flex">
+            <img src={HTML} alt='' />
+            <div className="app__testimonial-content">
+              <p className="p-text">Micael is an awesome developer! I am a backend developer with a passion for building beautiful and functional web applications.</p>
+              <div>
+                <h4 className="bold-text">Rahul</h4>
+                <h5 className="p-text">Corpodials</h5>
+              </div>
             </div>
           </div>
-        </div>
-
+          </SwiperSlide>
+        </Swiper>
       </div>
     </section>
   )
