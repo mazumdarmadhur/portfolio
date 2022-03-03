32980436d0a41755430f88d10e7669363910f36e
Asus_Sanjit <mazumdarmadhur@gmail.com>
2022-03-03 17:30:56 +0530

    Added Hooks for Thank You message.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,32 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './contact.scss'
-import Phone from '../../assets/mobile.png'
+import Linkedin from '../../assets/linkedin.png'
 import Email from '../../assets/email.png'
 import { useRef } from 'react';
 import emailjs from 'emailjs-com';
 
+
+const Result = () => {
+  return (
+    <h3 className="">
+      Thank you for getting in touch!
+    </h3>
+  )
+}
+
 function Contact() {
+  const [result, showResult] = useState(false);
+
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs.sendForm('service_4nf4skp', 'template_yby2vrb', form.current, 'wd9Vf7Wq5W8A0y6bv')
-    e.target.reset()
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.target.reset();
+    showResult(true);
   };
+
+//Hide Result
+setTimeout(() =>{
+  showResult(false);
+},5000);
+
   return (
     <section id='contact' className='app__footer'>
       <div className="contact__section">
-        <h2 className="head-text">Take A Coffe & Chat With Me</h2>
+        <h2 className="head-text">Take A Coffee & Chat With Me</h2>
 
         <div className="container footer__container">
           <div className="app__footer-cards">
             <div className="app__footer-card ">
               <img src={Email} alt="email" />
-              <a href="mailto:hello@micael.com" className="p-text">hello@micael.com</a>
+              <a href="mailto:mazumdarmadhur@gmail.com" className="p-text">hello@micael.com</a>
             </div>
             <div className="app__footer-card">
-              <img src={Phone} alt="phone" />
-              <a href="tel:+1 (123) 456-7890" className="p-text">+1 (123) 456-7890</a>
+              <img src={Linkedin} alt="linkedin" />
+              <a href="https://www.linkedin.com/in/sanjit-majumdar/" target='_blank' className="p-text">sanjit-majumdar@linkedin.com</a>
             </div>
           </div>
 
@@ -46,6 +69,10 @@ function Contact() {
               />
             </div>
             <button type="submit" className="p-text" >Send Message</button>
+
+            <div>
+              {result ? <Result /> : null}
+            </div>
           </form>
         </div>
 
