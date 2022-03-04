ef3f3fd3fe7ac4a3652efbfcf4802d552b468162
Asus_Sanjit <mazumdarmadhur@gmail.com>
2022-03-04 18:16:50 +0530

    Linked contact section in p tag

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
             I am an Endless seeker of Knowledge🧠. I speak, read and write in Hindi & English. I am constantly learning Web technologies and other design related topics, currently playing around with React👩🏾‍💻. <br />
             When I am not tapping the keys on the keyboard, you'll find me watching YouTube videos💻 or listening to music🎧. <br />
             <br />
-            I am fully committed to <span>creating</span> aesthetically <span>beautiful</span>, <span>responsive</span> and <span>usable products</span> 💻. I am skilled in many areas of design and development😎 and I am always excited to do business with like minded people, lets discuss over coffee.
+            I am fully committed to <span>creating</span> aesthetically <span>beautiful</span>, <span>responsive</span> and <span>usable products</span> 💻. I am skilled in many areas of design and development😎 and I am always excited to do business with like minded people, lets discuss over <a href="#contact">coffee</a>.
           </p>
         </div>
       </div>
