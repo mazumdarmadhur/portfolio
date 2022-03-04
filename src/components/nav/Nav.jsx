d93fe24f7708f0af8286bed6a7030bd5d929fce2
Asus_Sanjit <mazumdarmadhur@gmail.com>
2022-03-04 23:34:59 +0530

    Added animation to Nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import './nav.scss';
-import {BiHomeCircle} from 'react-icons/bi';
-import {TiUser} from 'react-icons/ti';
-import {RiServiceLine} from 'react-icons/ri';
-import {GiSkills} from 'react-icons/gi';
-import {RiContactsBook2Line} from 'react-icons/ri';
-import {useState} from 'react';
+import { BiHomeCircle } from 'react-icons/bi';
+import { TiUser } from 'react-icons/ti';
+import { RiServiceLine } from 'react-icons/ri';
+import { GiSkills } from 'react-icons/gi';
+import { RiContactsBook2Line } from 'react-icons/ri';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
 
 function Nav() {
   const [activeNav, setActiveNav] = useState('#home');
   return (
-    <nav>
-      <a href="#home" onClick={()=> setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><BiHomeCircle/></a>
-      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><TiUser/></a>
-      <a href="#skills" onClick={()=> setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><GiSkills/></a>
-      <a href="#testimonials" onClick={()=> setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}><RiServiceLine/></a>
-      <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiContactsBook2Line/></a>
-    </nav>
+    <motion.nav initial={{opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.8, ease: "easeOut" }}>
+      <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><BiHomeCircle /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><TiUser /></a>
+      <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><GiSkills /></a>
+      <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}><RiServiceLine /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiContactsBook2Line /></a>
+    </motion.nav>
   )
 }
 
