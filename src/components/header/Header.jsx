e117e484e964e905121798d125dc59f27c83ce77
Asus_Sanjit <mazumdarmadhur@gmail.com>
2022-03-04 15:14:44 +0530

    Header scroll down fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,7 @@ import ME from '../../assets/ME2.png';
 import HeaderSocials from './HeaderSocials';
 import Typewriter1 from './Typewriter';
 import CTA from './CTA';
+import { BsMouse } from 'react-icons/bs';
 
 function Header() {
   return (
@@ -11,10 +12,14 @@ function Header() {
       <div className='container header__container'>
         <p>Hello👋</p>
         <h2><Typewriter1 /></h2>
-        <CTA/>
+        <CTA />
         <HeaderSocials />
-        <a href="#about" className='scroll__down'>Scroll Down</a>
-
+        <a href="#about" className='scroll__down'>
+          <i className='scroll__icon'><BsMouse /></i>
+          <span className='scroll__name'>
+            Scroll Down
+          </span>
+        </a>
         <div className="me">
           <img src={ME} alt="me" />
         </div>
