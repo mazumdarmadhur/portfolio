b7321764a98a79f23d7f26ec3b23981d9cc3dc82
Asus_Sanjit <mazumdarmadhur@gmail.com>
2022-03-05 03:12:38 +0530

    Added animation in Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,27 +2,45 @@ import React from 'react';
 import ScrollToTop from './ScrollToTop';
 import './footer.scss';
 
+import { motion } from 'framer-motion';
+
+const bounceTransition = {
+  y: {
+    duration: .4,
+    yoyo: Infinity,
+    ease: "easeIn",
+  }
+};
+
 function Footer() {
   return (
     <footer>
-      <p className='footer__logo'>Sanjit Majumdar</p>
-      <ul className='permalinks'>
-        <li><a href="#">HOME</a></li>
-        <li><a href="#about">ABOUT</a></li>
-        <li><a href="#portfolio">PORTFOLIO</a></li>
-        <li><a href="#skills">SKILLS</a></li>
-        <li><a href="#testimonials">TESTIMONIAL</a></li>
-        <li><a href="#contact">CONTACT</a></li>
-      </ul>
+      <motion.p
+        transition={bounceTransition}
+        animate={{
+          y: ["0em", ".4em"],
+        }}
+        className='footer__logo'>Sanjit Majumdar</motion.p>
+      <motion.ul
+        whileInView={{ y: [100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className='permalinks'>
+        <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}><a href="#">HOME</a></motion.li>
+        <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}><a href="#about">ABOUT</a></motion.li>
+        <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}><a href="#portfolio">PORTFOLIO</a></motion.li>
+        <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}><a href="#skills">SKILLS</a></motion.li>
+        <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}><a href="#testimonials">TESTIMONIAL</a></motion.li>
+        <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}><a href="#contact">CONTACT</a></motion.li>
+      </motion.ul>
 
-      <div className="footer__socials">
-      </div>
-
-      <div className="footer__copyright">
+      <motion.div
+        whileInView={{ y: [100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="footer__copyright">
         <small><b>No &copy; copyright issues.</b></small>
         <p>Feel free to copy. If you need any help, ping me !</p>
-      </div>
-      <ScrollToTop/>
+      </motion.div>
+      <ScrollToTop />
     </footer>
   )
 }
--- src/components/footer/ScrollToTop.jsx
+++ src/components/footer/ScrollToTop.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { IoIosArrowUp } from "react-icons/io";
+import { motion } from 'framer-motion';
 
 function ScrollToTop() {
     const [visible, setVisible] = useState(false);
@@ -20,9 +21,23 @@ function ScrollToTop() {
     return (
         <>
             {visible && (
-                <div className="scrollToTop" onClick={scrollToTop}>
+                <motion.div
+                    animate={{
+                        scale: [1, 2, 2, 1, 1],
+                        rotate: [0, 0, 270, 270, 0],
+                        borderRadius: ["20%", "20%", "50%", "20%", "50%"]
+                    }}
+                    transition={{
+                        duration: 2,
+                        ease: "easeInOut",
+                        times: [0, 0.2, 0.5, 0.8, 1],
+                        repeat: Infinity,
+                        repeatDelay: 1
+                    }}
+                    className="scrollToTop"
+                    onClick={scrollToTop}>
                     <IoIosArrowUp />
-                </div>
+                </motion.div>
             )}
         </>
     )
