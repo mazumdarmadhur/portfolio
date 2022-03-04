4117f2b3233ab90b61558b0cdfdd34e4045662e6
Asus_Sanjit <mazumdarmadhur@gmail.com>
2022-03-05 02:28:13 +0530

    Added animation in Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,13 +4,15 @@ import Linkedin from '../../assets/linkedin.png'
 import Email from '../../assets/email.png'
 import { useRef } from 'react';
 import emailjs from 'emailjs-com';
+import { motion } from 'framer-motion';
 
 
 const Result = () => {
   return (
-    <h3 className="">
-      Thank you for getting in touch!
-    </h3>
+    <motion.h3
+      exit={{ y: -1000 }}>
+      Thank you for getting in <span>touch!</span>
+    </motion.h3>
   )
 }
 
@@ -31,18 +33,24 @@ function Contact() {
     showResult(true);
   };
 
-//Hide Result
-setTimeout(() =>{
-  showResult(false);
-},5000);
+  //Hide Result
+  setTimeout(() => {
+    showResult(false);
+  }, 5000);
 
   return (
     <section id='contact' className='app__footer'>
       <div className="contact__section">
-        <h2 className="head-text">Take A Coffee & <span>Chat With Me</span></h2>
+        <motion.h2
+          whileInView={{ y: [100, 0], opacity: [0, 1] }}
+          transition={{ duration: 0.5 }}
+          className="head-text">Take A Coffee & <span>Chat With Me</span></motion.h2>
 
         <div className="container footer__container">
-          <div className="app__footer-cards">
+          <motion.div
+            whileInView={{ y: [100, 0], opacity: [0, 1] }}
+            transition={{ duration: 0.5 }}
+            className="app__footer-cards">
             <div className="app__footer-card ">
               <img src={Email} alt="email" />
               <a href="mailto:hey@sanjitmajumdar.com" className="p-text">hey@sanjitmajumdar.com</a>
@@ -51,28 +59,42 @@ setTimeout(() =>{
               <img src={Linkedin} alt="linkedin" />
               <a href="https://www.linkedin.com/in/sanjit-majumdar/" target='_blank' className="p-text">sanjit-majumdar@linkedin.com</a>
             </div>
-          </div>
+          </motion.div>
 
           <form ref={form} onSubmit={sendEmail} className="app__footer-form app__flex">
-            <div className="app__flex">
+            <motion.div
+              whileInView={{ y: [100, 0], opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__flex">
               <input className="p-text" type="text" placeholder="Your Name" name="name" required />
-            </div>
-            <div className="app__flex">
+            </motion.div>
+            <motion.div
+              whileInView={{ y: [100, 0], opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__flex">
               <input className="p-text" type="email" placeholder="Your Email" name="email" required />
-            </div>
-            <div>
+            </motion.div>
+            <motion.div whileInView={{ y: [100, 0], opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}>
               <textarea
                 className="p-text"
                 placeholder="Your Message"
                 name="message"
                 required
               />
-            </div>
-            <button type="submit" className="p-text" >Send Message</button>
+            </motion.div>
+            <motion.button
+              whileInView={{ y: [100, 0], opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              type="submit"
+              className="p-text" >Send Message</motion.button>
 
-            <div>
+            <motion.div className='TQ_Msg'
+              whileInView={{ y: [100, 0], opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              exit={{ y: -1000 }}>
               {result ? <Result /> : null}
-            </div>
+            </motion.div>
           </form>
         </div>
 
